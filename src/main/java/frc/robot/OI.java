e0aee9034ca19931d3ca56fd7614d581e4a0d0ff
Sid Thomas <thomas.siddharth@gmail.com>
2023-09-12 19:22:55 -0700

    [fix] Invert pigeon pitch

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -11,5 +11,10 @@ public class OI {
 
   // In proper orientation, Pigeon is flat and facing so that X-axis is forward
   /** Roll increases to the right, pitch to the front, and yaw counter-clockwise. */
-  public final static Pigeon2 pigeon = new Pigeon2(DeviceConstants.kPigeonID);
+  public final static Pigeon2 pigeon = new Pigeon2(DeviceConstants.kPigeonID) {
+    @Override
+    public double getPitch() {
+      return -getPitch();
+    };
+  };
 }
