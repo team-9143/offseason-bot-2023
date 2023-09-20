dcf96db9186644891642ad5bd9f80854009984b5
Armor Robotics <robotics@riordanhs.org>
2023-09-19 20:45:50 -0700

    [hotfix] Drive PID tuning 9/19

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -82,25 +82,25 @@ public final class Constants {
       kSwerve_fl = new SwerveModuleConstants(
         41, 42, 43, 261.229, false,
         new Translation2d(0.22225, 0.22225),
-        new PIDController(0.02, 0.01, 0),
+        new PIDController(0.2, 0, 0),
         new PIDController(0.0100, 0, 0)
       ),
       kSwerve_fr = new SwerveModuleConstants(
         11, 12, 13, -150.029, false,
         new Translation2d(0.22225, -0.22225),
-        new PIDController(0.02, 0.01, 0),
+        new PIDController(0.2, 0, 0),
         new PIDController(0.0100, 0, 0)
       ),
       kSwerve_bl = new SwerveModuleConstants(
         31, 32, 33, 301.465, false,
         new Translation2d(-0.22225, 0.22225),
-        new PIDController(0.02, 0.01, 0),
+        new PIDController(0.2, 0, 0),
         new PIDController(0.0100, 0, 0)
       ),
       kSwerve_br = new SwerveModuleConstants(
         21, 22, 23, -3.428, false,
         new Translation2d(-0.22225, -0.22225),
-        new PIDController(0.02, 0.01, 0),
+        new PIDController(0.2, 0, 0),
         new PIDController(0.0100, 0, 0)
       );
   }
